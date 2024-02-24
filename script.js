0bf72f26b7c74e8586bfc553cac8bed5550522f0
Venu Virparia <129399359+flute-v@users.noreply.github.com>
2024-02-24 19:27:03 +0530

    Update script.js
    Preview function

--- script.js
+++ script.js
@@ -8,6 +8,22 @@ function toggleConversionOptions() {
   formatSelect.disabled = !document.getElementById("convertCheckbox").checked;
 }
 
+function previewImage(event) {
+  var input = event.target;
+  var image = document.getElementById("preview");
+
+  if (input.files && input.files[0]) 
+  {
+    var reader = new FileReader();
+    reader.onload = function (e) 
+    {
+      image.src = e.target.result;
+    };
+
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+
 function processImage() {
   var fileInput = document.getElementById("imageInput");
 
