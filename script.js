a6e8e1280d269fd3874da073df1e2bc1cfa2b639
NandRabadiya <124863915+NandRabadiya@users.noreply.github.com>
2024-02-24 15:02:17 +0530

    Update script.js
    Process function in making

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ function toggleConversionOptions() {
   var formatSelect = document.getElementById("formatSelect");
   formatSelect.disabled = !document.getElementById("convertCheckbox").checked;
 }
+
+function processImage() {
+  var fileInput = document.getElementById("imageInput");
+
+  var compressCheckbox = document.getElementById("compressCheckbox");
+  var convertCheckbox = document.getElementById("convertCheckbox");
+
+  var startButton = document.getElementById("startButton");
+  var downloadButton = document.getElementById("downloadButton");
+
+  var imageFile = fileInput.files[0];
+  var inputSize = imageFile.size / 1000;
+  //console.log("Original size: " + inputSize);
+  var compressionQuality = 100; // Default compression quality
+  var expectedSize = inputSize;
+  
+//compression and conversion logic here
+}
